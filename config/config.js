6b9d19588255afa2a6c409ffa92b899da403cf64
FEAprince <104749623+FEAprince@users.noreply.github.com>
2022-07-22 18:37:32 +0530

    ass[]

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    BASEURL:  "https://fea-backend.herokuapp.com",
+    email: {
+      EMAILUSERNAME: "vansh.frontendarmy@gmail.com",
+      EMAILPASSWORD: "itdurvloxvlukyeq",
+      SENDMAILFROM: "vansh.frontendarmy@gmail.com",
+    },
+    mogno: {
+      MONGO_USERNAME: 'vansh',
+      MONGO_PASSWORD: 'vansh123',
+      MONGO_DBNAME: 'Ecommerce',
+      MONGO_HOST: 'cluster0.bdfg5vk.mongodb.net',
+  },
+    mongoDB: "mongodb+srv://vansh:V%40nsh1316@cluster0.bdfg5vk.mongodb.net/test",
+    emailSubject: {
+      welcome: "Welcome to our Ecommerece",
+    },
+  
+    jwt: {
+      JWTSECRETKEY: "BACKEND$frontendarmy@2002",
+      JWTEXPIRY: "1h",
+    },
+  };
+  
